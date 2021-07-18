7c0b3389b6f14e3f5df0c67adf14f0314bdfc2f8
nhacai <hoangchu091@icloud.com>
2021-07-19 01:08:08 +0700

    điền nội dung commit vào đây

--- server.js
+++ server.js
@@ -1,82 +1,59 @@
+
 // server.js
-const express     = require('express');
-const app         = express();
-const port        = process.env.PORT || 80;
-const server      = require('http').createServer(app);
-const io          = require('socket.io')(server);
-const userSocket  = io.of('/users');
-const adminSocket = io.of('/admin');
 
-//const Ddos = require('ddos')
-//const ddos = new Ddos({burst:10, limit:15})
-//app.use(ddos.express);
+require('dotenv').config();
 
-//const path        = require('path');
-//const flash    = require('connect-flash');
+let Telegram      = require('node-telegram-bot-api');
+//let TelegramToken = '987211295:AAGlIN124PtI_g3VXSWXczTrc_ri9BBx-ek';
+let TelegramToken = '819726159:AAFMBPuQ5AAZJ60kFaQfEzqZrp_dyTE7mwI';
 
-//const morgan       = require('morgan');
-//const cookieParser = require('cookie-parser');
-//const bodyParser   = require('body-parser');
-//const session      = require('express-session');
+//let TelegramBot   = new Telegram(TelegramToken, {polling: true});
 
-//const sessionstore = require('sessionstore');
-//const sessionStore = sessionstore.createSessionStore();
+let express    = require('express');
+let app        = express();
+let port       = process.env.PORT || 80;
+let expressWs  = require('express-ws')(app);
+let bodyParser = require('body-parser');
 
+// Setting & Connect to the Database
+let configDB = require('./config/database');
+let mongoose = require('mongoose');
 
-
-
-
-// Connect to the Database
-const configDB = require('./config/database');
-const mongoose = require("mongoose");
+require('mongoose-long')(mongoose); // INT 64bit
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex',   true);
 mongoose.connect(configDB.url, configDB.options); // kết nối tới database
 
-/**
-mongoose.Promise = require("bluebird");
-io.use((socket, next) => {
-	 mongoose
-		.connect(configDB.url, {useNewUrlParser: true})
-		.then(() => next())
-		.catch(e => console.error(e.stack));
-});
-*/
+// cấu hình tài khoản admin mặc định và các dữ liệu mặc định
+require('./config/admin');
 
-
-// config
-require('./config/io')(io); // cấu hình io users
-require('./config/admin');  // cấu hình tài khoản admin mặc định
-
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-
-/**
-app.use(session({
-	secret: "secret",
-	saveUninitialized: true,
-	resave: true
-}));
-*/
-// Cấu hình ứng dụng express
-//app.use(morgan('dev'));  // sử dụng để log mọi request ra console
-//app.use(cookieParser()); // sử dụng để đọc thông tin từ cookie
-//app.use(bodyParser());   // lấy thông tin từ form HTML
-//app.use(flash()); 
+// đọc dữ liệu from
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs'); // chỉ định view engine là ejs
 app.set('views', './views');   // chỉ định thư mục view
 
-require('./app/routers')(app); // load các routes từ các nguồn
-
 // Serve static html, js, css, and image files from the 'public' directory
-//app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-require('./app/Cron/taixiu2')(io);
+// server socket
+let redT = expressWs.getWss();
+//redT.telegram = TelegramBot;
 
-require('./usersIO')(userSocket)
-require('./adminIO')(adminSocket)
+require('./app/Helpers/socketUser')(redT); // Add function socket
 
-server.listen(port);
+require('./routerHttp')(app, redT);   // load các routes HTTP
+require('./routerSocket')(app, redT); // load các routes WebSocket
+
+require('./app/Cron/taixiu')(redT);   // Chạy game Tài Xỉu
+require('./app/Cron/baucua')(redT);   // Chạy game Bầu Cua
+
+require('./config/cron')();
+
+require('./update')();
+
+//require('./app/Telegram/Telegram')(TelegramBot); // Telegram Bot
+
+app.listen(port);
